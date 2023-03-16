81e8f96fe9dbd515997c5cc062b3c48bf60453fd
Richard Tichý <tichy@artify.zone>
2023-03-16 17:33:18 +0100

    Implement solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,26 @@
 fun main() {
-    println("Hello world!")
+    (1..(readln().toInt())).forEach { _ ->
+        val n = readln().toInt()
+        val a = readln().split(' ').map { it.toInt() }
+
+        var prev_point = 0
+        var found = 0
+
+        run {
+            var i = 0
+
+            while (prev_point < 1440) {
+                val next = if (i < n) a[i] else 1440
+                if (prev_point + 120 <= next) {
+                    found++
+                    prev_point += 120
+                } else {
+                    prev_point = next
+                    i++
+                }
+            }
+        }
+
+        println(if(2 <= found) "YES" else "NO")
+    }
 }
